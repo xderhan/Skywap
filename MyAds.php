d3d9fb8eacd088b8aec1e7f045c1c334ff886b58
Sederhana Gulo <admin@x-coder.mobi>
2012-02-08 23:07:26 +0700

    Add the ads link

--- MyAds.php
+++ MyAds.php
@@ -1,3 +1,9 @@
 <?php
-return array();
-?>
+
+return array (
+	'<a href="http://imgrox.com/x/0/3935/30165/hitcross/" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/271/ATT_Banners_v2_300x50.gif" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=271&aff_id=9533&aff_sub=hitcross&file_id=3201" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH18u" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/420/Mobile_300x50.gif" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=420&aff_id=9533&aff_sub=hitcross&file_id=3632" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH192" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/398/300x50-amazon-2.gif" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=398&aff_id=9533&aff_sub=hitcross&file_id=3333" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH196" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/344/300x50_A3_static.png" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=344&aff_id=9533&aff_sub=hitcross&file_id=2950" width="1" height="1">',
+	'<a href="http://mob4fun.info" target="_blank"><img src="http://mob4fun.info/images/banner.png" width="216" height="36" border="0" /></a>',
+);
