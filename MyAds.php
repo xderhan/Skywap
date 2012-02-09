196e99338c3ef632f23bb1088d0344ce0442457a
Sederhana Gulo <admin@x-coder.mobi>
2012-02-10 04:00:10 +0700

    Remove Lesko offer :(

--- MyAds.php
+++ MyAds.php
@@ -1,10 +1,10 @@
 <?php
 
 return array (
-	'<a href="http://tm.trackmobi.com/SH1Fg" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/153/govt-320-48.jpg" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=153&aff_id=9533&aff_sub=htc82&file_id=10507" width="1" height="1">',
-	'<a href="http://tm.trackmobi.com/SH18u" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/420/Mobile_300x50.gif" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=420&aff_id=9533&aff_sub=hitcross&file_id=3632" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH1GO" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/315/320x50.gif" width="320" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=315&aff_id=9533&aff_sub=htc102&file_id=2260" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH1GY" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/460/1.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=460&aff_id=9533&aff_sub=htc102&file_id=5870" width="1" height="1">',
 	'<a href="http://tm.trackmobi.com/SH1Fc" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25RingtonesNow_WhBkPk_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc82&file_id=10410" width="1" height="1">',
-	'<a href="http://tm.trackmobi.com/SH196" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/344/300x50_A3_static.png" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=344&aff_id=9533&aff_sub=hitcross&file_id=2950" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH1GG" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/344/300x50_static.png" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=344&aff_id=9533&aff_sub=htc102&file_id=2970" width="1" height="1">',
 	'<a href="http://mob4fun.info" target="_blank"><img src="http://mob4fun.info/images/banner.png" width="216" height="36" border="0" /></a>',
 	'<a href="http://tm.trackmobi.com/SH1Fk" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/458/3.jpg" width="216" height="36" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=458&aff_id=9533&aff_sub=htc82&file_id=5736" width="1" height="1">',
 );
