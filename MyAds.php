c2d66485818b000627551a1a7b73a51752821a0f
Sederhana Gulo <admin@x-coder.mobi>
2012-03-14 00:48:57 +0700

    Focus on one offer will not work for us, changed the offers to the many work offers

--- MyAds.php
+++ MyAds.php
@@ -1,15 +1,14 @@
 <?php
 
 return array (
-<<<<<<< HEAD
-    '<a href="http://tm.trackmobi.com/SH1Fc" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25RingtonesNow_WhBkPk_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc82&file_id=10410" width="1" height="1">',
-=======
-    '<a href="http://tm.trackmobi.com/SH1LC" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/498/govt-300-50.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=498&aff_id=9533&aff_sub=htc182&file_id=5870" width="1" height="1">',
-	'<a href="http://tm.trackmobi.com/SH1GY" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/460/1.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=460&aff_id=9533&aff_sub=htc102&file_id=5870" width="1" height="1">',
-	'<a href="http://tm.trackmobi.com/SH1Fc" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25RingtonesNow_WhBkPk_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc82&file_id=10410" width="1" height="1">',
->>>>>>> parent of e9af2f1... Remove uncoverted ads
-	'<a href="http://mob4fun.info" target="_blank"><img src="http://mob4fun.info/images/banner.png" width="216" height="36" border="0" /></a>',
-    '<a href="http://tm.trackmobi.com/SH1Ny" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25Ringtones_WhBkBl_DN_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc232&file_id=10386" width="1" height="1">',
-    '<a href="http://tm.trackmobi.com/SH1Va" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_YouHave25Bonus_WhBkRd_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc133_WhBkRd_CH&file_id=10430" width="1" height="1">',
-    '<a href="http://tm.trackmobi.com/SH1Ve" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_YouHave25Ringtones_WhBlBkOg_DN_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc133_WhBlBkOg_DN&file_id=10450" width="1" height="1">',
+    '<a href="http://tm.trackmobi.com/SH1Vq" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/498/govt-300-50.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=498&aff_id=9533&aff_sub=htc143" width="1" height="1">',
+
+    '<a href="http://mob4fun.info" target="_blank"><img src="http://mob4fun.info/images/banner.png" width="216" height="36" border="0" /></a>',
+
+    '<a href="http://tm.trackmobi.com/SH1Vu" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25RingtonesNow_WhBkPk_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc143_WhBkPk_CH_320x48&file_id=10410" width="1" height="1">',
+    '<a href="http://tm.trackmobi.com/SH1Vy" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_YouHave25Ringtones_WhBlBkOg_DN_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc143_WhBlBkOg_DN_320x48&file_id=10450" width="1" height="1">',
+
+    '<a href="http://tm.trackmobi.com/SH1W2" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/458/3.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=458&aff_id=9533&aff_sub=htc143&file_id=5742" width="1" height="1">',
+
+    '<a href="http://tm.trackmobi.com/SH1W4" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/460/1.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=460&aff_id=9533&aff_sub=htc143&file_id=5870" width="1" height="1">',
 );
