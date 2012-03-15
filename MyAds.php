73aa98b6328797fa8a57e252362f0cfb88b4ce4f
Sederhana Gulo <admin@x-coder.mobi>
2012-03-15 22:16:59 +0700

    Reset to working converted offers

--- MyAds.php
+++ MyAds.php
@@ -1,16 +1,10 @@
 <?php
 
 return array (
-    '<a href="http://tm.trackmobi.com/SH1Vq" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/498/govt-300-50.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=498&aff_id=9533&aff_sub=htc143" width="1" height="1">',
-
-    '<a href="http://mob4fun.info" target="_blank"><img src="http://mob4fun.info/images/banner.png" width="216" height="36" border="0" /></a>',
-
-    '<a href="http://tm.trackmobi.com/SH1Vu" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25RingtonesNow_WhBkPk_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc143_WhBkPk_CH_320x48&file_id=10410" width="1" height="1">',
-    '<a href="http://tm.trackmobi.com/SH1Vy" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_YouHave25Ringtones_WhBlBkOg_DN_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc143_WhBlBkOg_DN_320x48&file_id=10450" width="1" height="1">',
-
-    '<a href="http://tm.trackmobi.com/SH1W2" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/458/3.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=458&aff_id=9533&aff_sub=htc143&file_id=5742" width="1" height="1">',
-
-    '<a href="http://tm.trackmobi.com/SH1W4" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/460/1.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=460&aff_id=9533&aff_sub=htc143&file_id=5870" width="1" height="1">',
-
-    '<a href="http://adscendmedia.com/click.php?aff=11783&camp=6717&crt=15346&sid="><img src="http://adscendmedia.com/creative.php?id=15346" border="0"></a>',
+    '<a href="http://tm.trackmobi.com/SH1Lm" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/498/govt-300-50.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=498&aff_id=9533&aff_sub=htc182" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH1GY" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/460/1.jpg" width="300" height="50" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=460&aff_id=9533&aff_sub=htc102&file_id=5870" width="1" height="1">',
+	'<a href="http://tm.trackmobi.com/SH1Fc" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/476/Motime_25RingtonesNow_WhBkPk_CH_320x48.gif" width="320" height="48" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=476&aff_id=9533&aff_sub=htc82&file_id=10410" width="1" height="1">',
+	'<a href="http://mob4fun.info" target="_blank"><img src="http://mob4fun.info/images/banner.png" width="216" height="36" border="0" /></a>',
+	'<a href="http://tm.trackmobi.com/SH1Fk" target="_blank"><img src="http://media.go2speed.org/brand/files/offermobi/458/3.jpg" width="216" height="36" border="0" /></a><img src="http://tm.trackmobi.com/aff_i?offer_id=458&aff_id=9533&aff_sub=htc82&file_id=5736" width="1" height="1">',
 );
+
